cde846492cb4019b7ad05b8c90d19fa5a50c9a6b
Yannina Fariña <91395029+YanninaF@users.noreply.github.com>
2024-07-30 10:59:45 -0400

    17-Creación de tabla:
    Escribe una consulta SQL para crear una tabla Usuarios con columnas id y nombre.

---
+++ 17_Creaciontabla.sql
@@ -0,0 +1,6 @@
+--Creación de tabla: Escribe una consulta SQL para crear una tabla Usuarios con columnas id y nombre.
+CREATE TABLE Usuarios(
+id_usuario SERIAL PRIMARY KEY,
+nombre VARCHAR(30)
+);
+
