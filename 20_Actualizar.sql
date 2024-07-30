e0199f78f98903c8527613d24933bdec469b6c7f
Yannina Fariña <91395029+YanninaF@users.noreply.github.com>
2024-07-30 11:01:27 -0400

    20-Actualizar registros:
    Escribe una consulta SQL para actualizar el nombre de un usuario específico en la tabla Usuarios.

---
+++ 20_Actualizar.sql
@@ -0,0 +1,4 @@
+--20-Actualizar registros: Escribe una consulta SQL para actualizar el nombre de un usuario específico en la tabla Usuarios.
+UPDATE Usuarios
+SET nombre = 'Yannina'
+WHERE nombre = 'Yannina Elizabeth'
